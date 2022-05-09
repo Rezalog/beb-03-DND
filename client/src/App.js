6044ecaf65b9f03ed405527dbb5b3e49c15a4459
JJakDDo <kangdw124@gmail.com>
2022-05-09 13:14:17 +0900

    feat(강동욱): add caverjs and webpack config

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,28 @@
 import "./App.css";
 import game from "./PhaserGame";
+import Caver from "caver-js";
 
 function App() {
-  return <div className='App'></div>;
+  const connectToWallet = async () => {
+    if (typeof window.klaytn !== "undefined") {
+      const provider = window["klaytn"];
+      try {
+        const accounts = await window.klaytn.enable();
+        const account = window.klaytn.selectedAddress;
+
+        const caver = new Caver(window.klaytn);
+        const balance = await caver.klay.getBalance(account);
+        console.log(balance);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+  return (
+    <div className='App'>
+      <button onClick={connectToWallet}>지갑 연결</button>
+    </div>
+  );
 }
 
 export default App;
