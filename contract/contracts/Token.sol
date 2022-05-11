5f7f01fdcf3b43d12e41cea2be514db82925bc3f
BEB03 <cpt9136@gmail.com>
2022-05-11 13:00:22 +0900

    feat(백동현): deploy kip-7 and make dex feature(swap)

---
+++ contract/contracts/Token.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.6;
+
+import "@klaytn/contracts/token/KIP7/KIP7.sol";
+import "@klaytn/contracts/token/KIP7/KIP7Metadata.sol";
+
+contract Token is KIP7, KIP7Metadata {
+  constructor(
+    string memory name,
+    string memory symbol,
+    uint8 decimals,
+    uint256 initialSupply
+  ) KIP7Metadata(name, symbol, decimals) public {
+    _mint(msg.sender, initialSupply);
+  }
+}
