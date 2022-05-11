ff34a14636610fe1445733f316fe03608b0fb381
BEB03 <cpt9136@gmail.com>
2022-05-11 13:08:37 +0900

    feat(백동현): deploy kip-7 and make dex feature(swap)

--- contract/migrations/1_initial_migration.js
+++ contract/migrations/1_initial_migration.js
@@ -7,5 +7,5 @@ const value = "10000000000000000000000";
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
   deployer.deploy(Exchange, process.env.TOKEN_CONTRACT);
-  deployer.deploy(Token, "URUtoken", "URU", 18, value);
+  // deployer.deploy(Token, "URUtoken", "URU", 18, value);
 };
