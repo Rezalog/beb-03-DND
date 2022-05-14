851674eba3ebd7c79751eedadc66a99f3dbd0888
Woohyun Yang <woohyunyang@Woohyunui-MacBookAir.local>
2022-05-15 05:33:26 +0900

    feat(양우현): Modify SignIn, SignUp to connect Server

--- server/index.js
+++ server/index.js
@@ -1,29 +1,30 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const mongoose = require('mongoose');
-const usersRouter = require('./routes/users');
-const { PORT, USER_NAME, USER_PASSWORD, CLOUD_NAME, DATABASE_NAME } = process.env;
+const mongoose = require("mongoose");
+const usersRouter = require("./routes/users");
+const { PORT, USER_NAME, USER_PASSWORD, CLOUD_NAME, DATABASE_NAME } =
+  process.env;
 
-const MONGO_URI = 
-`mongodb+srv://${USER_NAME}:${USER_PASSWORD}@${CLOUD_NAME}.xhvhs.mongodb.net/${DATABASE_NAME}`
+const MONGO_URI = `mongodb+srv://${USER_NAME}:${USER_PASSWORD}@${CLOUD_NAME}.xhvhs.mongodb.net/${DATABASE_NAME}`;
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log('MongoDB connected...'))
-.catch(error => console.log(error));
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((error) => console.log(error));
 
 app.use(
-	cors({
-		origin: "http://localhost:3000",
-	})
+  cors({
+    origin: "http://localhost:3000",
+  })
 );
-app.use(express.static('public'))
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/users', usersRouter);
+app.use("/users", usersRouter);
 
 app.get("/", (req, res) => {});
 app.listen(PORT, () => {
-	console.log(`Listening...on ${PORT}`);
+  console.log(`Listening...on ${PORT}`);
 });
