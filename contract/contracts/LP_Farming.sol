008deea4e1b14bbd67c4a81ff5eb2f95b90bc1f6
BEB03 <cpt9136@gmail.com>
2022-05-16 16:41:25 +0900

    feat(백동현): improve LP-Farming : istribution method

--- contract/contracts/LP_Farming.sol
+++ contract/contracts/LP_Farming.sol
@@ -7,6 +7,9 @@ import "@klaytn/contracts/token/KIP7/KIP7Mintable.sol";
 import "./Token.sol";
 
 contract LP_Farming {
+    
+    // iterations을 위한 배열 선언
+    address[] public userList;
 
     // 관련 정보를 기록할 mapping 선언
     mapping(address => uint256) public stakingBalance;
@@ -26,7 +29,7 @@ contract LP_Farming {
     constructor (
         Token _token
         ) public {
-            LP_Token = 0x87E8Df6A461B0451A09bbc1FCc053b47d2dB5A4d; // lp 토큰 주소 (예치)
+            LP_Token = 0x28F6221ED6F6e23C4943Bb5910E2F6BAb6F49470; // lp 토큰 주소 (예치)
             token = _token;
         }
 
@@ -37,16 +40,34 @@ contract LP_Farming {
             KIP7(LP_Token).balanceOf(msg.sender) >= amount, 
             "토큰이 부족합니다");
 
-        if(isStaking[msg.sender] == true){
-            uint256 toTransfer = calculateYieldTotal(msg.sender);
-            URUBalance[msg.sender] += toTransfer;
+        // if(isStaking[msg.sender] == true){
+        //     uint256 toTransfer = calculateYieldTotal(msg.sender);
+        //     URUBalance[msg.sender] += toTransfer;
+        // }
+
+        // userList에 "없는 주소일 경우" msg.sender을 주소를 userList에 삽입
+        // 처음 스테이킹 시 URUBalance가 알수 없는 값으로 설정되어 있어 0으로 고정 (해결중)
+        if(isStaking[msg.sender] == false) {
+            userList.push(msg.sender);
+            URUBalance[msg.sender] = 0;
         }
 
         KIP7(LP_Token).transferFrom(msg.sender, address(this), amount);
         stakingBalance[msg.sender] += amount;
-        startTime[msg.sender] = block.timestamp;
+        // startTime[msg.sender] = block.timestamp;
         isStaking[msg.sender] = true;
         emit Stake(msg.sender, amount);
+
+        // userList에 있는 주소들에 대하여 현재까지 쌓인 이자를 URUBalance에 저장해 주어야 한다.
+        // 예치된 이 시점에 contribute가 변화하기 때문임
+        // 즉 userList들의 주소들에 대하여 각각 calculateYieldTotal을 해준 후 그 값을 URU밸런스에 추가한다. 
+        // Starttime도 초기화 해야한다.
+
+        for (uint256 i = 0; i < userList.length; i++) {
+            uint256 nowURUBalance = calculateYieldTotal(userList[i]);
+            URUBalance[userList[i]] += nowURUBalance;
+            startTime[userList[i]] = block.timestamp;
+        }
     }
 
     // 예치량 빼기
@@ -65,8 +86,20 @@ contract LP_Farming {
         URUBalance[msg.sender] += yieldTransfer;
         if(stakingBalance[msg.sender] == 0){
             isStaking[msg.sender] = false;
+            // 예치량이 0이되면 userList에서 해당 함수의 주소를 삭제
         }
         emit Unstake(msg.sender, balTransfer);
+
+        for (uint256 i = 0; i < userList.length; i++) {
+        startTime[userList[i]] = block.timestamp;
+        uint256 nowURUBalance = calculateYieldTotal(userList[i]);
+        URUBalance[userList[i]] += nowURUBalance;
+        }
+    }
+
+    // 현재 스테이킹 중인 address를 배열로 반환
+    function getuserList() public view returns(address[] memory) {
+        return userList;
     }
 
     // 마지막 스테이킹 변화로부터 시간 측정 (블록타임 활용)
@@ -77,13 +110,15 @@ contract LP_Farming {
     }
 
     // 전체 비율 중 해당 유저의 유동성 기여도 측정
+    // 소수점 둘째 자리까지 가능
     function calculateContribute(address user) public view returns(uint256){
-        uint contribution = stakingBalance[user] / KIP7(LP_Token).balanceOf(address(this)) * 10**18;
+        uint contribution = (stakingBalance[user] * 100) / KIP7(LP_Token).balanceOf(address(this));
         return contribution;
     }
 
-    // 시간과 기여도의 곱으로 지급할 이자를 계산
-    // 총 분당 1500개, 초당 25개씩 지급
+    // 새로운 기간 동안 (새로운 stake나 unstake가 일어나지 않는 동안) 각 유저에게 지급할 이자를 계산
+    // 시간과 기여도의 곱으로 계산
+    // 총 분당 1500개, 초당 25개씩 발행될 것이고 이를 기여도에 따라서 나누는 시스템
     function calculateYieldTotal(address user) public view returns(uint256) {
         uint256 time = calculateYieldTime(user);
         uint256 contribution = calculateContribute(user);
@@ -91,6 +126,7 @@ contract LP_Farming {
         return rawYield;
     } 
 
+    // URUBalance에 저장된 값과 현재 기간 쌓인 이자의 합으로 mint해줌
     function withdrawYield() public {
         uint256 toTransfer = calculateYieldTotal(msg.sender);
 
