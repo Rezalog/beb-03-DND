0045b62640dbc12bfca643b67ded17411404cc8e
BEB03 <cpt9136@gmail.com>
2022-05-16 16:57:19 +0900

    feat(백동현): improve LP-Farming : distribution method

--- contract/contracts/LP_Farming.sol
+++ contract/contracts/LP_Farming.sol
@@ -40,29 +40,18 @@ contract LP_Farming {
             KIP7(LP_Token).balanceOf(msg.sender) >= amount, 
             "토큰이 부족합니다");
 
-        // if(isStaking[msg.sender] == true){
-        //     uint256 toTransfer = calculateYieldTotal(msg.sender);
-        //     URUBalance[msg.sender] += toTransfer;
-        // }
-
         // userList에 "없는 주소일 경우" msg.sender을 주소를 userList에 삽입
-        // 처음 스테이킹 시 URUBalance가 알수 없는 값으로 설정되어 있어 0으로 고정 (해결중)
         if(isStaking[msg.sender] == false) {
             userList.push(msg.sender);
-            URUBalance[msg.sender] = 0;
         }
 
         KIP7(LP_Token).transferFrom(msg.sender, address(this), amount);
         stakingBalance[msg.sender] += amount;
-        // startTime[msg.sender] = block.timestamp;
+        startTime[msg.sender] = block.timestamp;
         isStaking[msg.sender] = true;
         emit Stake(msg.sender, amount);
 
-        // userList에 있는 주소들에 대하여 현재까지 쌓인 이자를 URUBalance에 저장해 주어야 한다.
-        // 예치된 이 시점에 contribute가 변화하기 때문임
-        // 즉 userList들의 주소들에 대하여 각각 calculateYieldTotal을 해준 후 그 값을 URU밸런스에 추가한다. 
-        // Starttime도 초기화 해야한다.
-
+        // stake가 일어날 때 마다 모든 유저에 대해서 해당 시각의 이자를 mapping 결과에 저장
         for (uint256 i = 0; i < userList.length; i++) {
             uint256 nowURUBalance = calculateYieldTotal(userList[i]);
             URUBalance[userList[i]] += nowURUBalance;
@@ -90,6 +79,7 @@ contract LP_Farming {
         }
         emit Unstake(msg.sender, balTransfer);
 
+        // stake가 일어날 때와의 마찬가지의 논리
         for (uint256 i = 0; i < userList.length; i++) {
         startTime[userList[i]] = block.timestamp;
         uint256 nowURUBalance = calculateYieldTotal(userList[i]);
@@ -110,7 +100,7 @@ contract LP_Farming {
     }
 
     // 전체 비율 중 해당 유저의 유동성 기여도 측정
-    // 소수점 둘째 자리까지 가능
+    // 소수점 둘째 자리까지 가능 (백분율)
     function calculateContribute(address user) public view returns(uint256){
         uint contribution = (stakingBalance[user] * 100) / KIP7(LP_Token).balanceOf(address(this));
         return contribution;
