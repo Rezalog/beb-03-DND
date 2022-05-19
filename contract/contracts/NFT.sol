5d5af9475deb7814e0d0995d13ad3a19d7a30aef
Woohyun Yang <woohyunyang@Woohyunui-MacBookAir.local>
2022-05-19 15:11:13 +0900

    feat(양우현): Modify NFT contract

--- contract/contracts/NFT.sol
+++ contract/contracts/NFT.sol
@@ -3,136 +3,198 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "../node_modules/@klaytn/contracts/token/KIP17/KIP17Full.sol";
 import "../node_modules/@klaytn/contracts/drafts/Counters.sol";
+import "./Token.sol";
 
 contract NFT is KIP17Full{
   using Counters for Counters.Counter;
   Counters.Counter private _Ids;
 
-  constructor() public KIP17Full("DND-WEAPON", "WEAPON"){}
+    uint256 weaponDigits = 16;
+    uint256 weaponModulus = 10 ** weaponDigits;
+    
+    uint256 [9] public percentage; // 합성 성공확률
+    uint256 [9] public tokenToCompound; // 합성시 토큰 소모량
+    uint256 [9] public tokenToFixDurability; // 내구도 수리시 토큰 소모량
+    uint256 [9] public tokenToFixEnchant; // 강화 내구도 수리시 토큰 소모량
+    
 
-  function mint (address recipient) public returns (uint256) {
+    Token token;
+
+    struct Weapon {
+        uint256 weaponType; // dna 
+        uint256 weaponLevel; // 
+        uint256 durability; // 내구도 - staking
+        uint256 enchant; // 강화횟수 - compound
+    }
+
+    Weapon[] public weapons;
+
+    event NewWeapon(uint256 weaponId, uint256 weaponType, uint256 weaponLevel);
+
+
+    constructor(address _token) public KIP17Full("DND-WEAPON", "WEAPON"){
+        percentage[0] = 99;
+        percentage[1] = 79;
+        percentage[2] = 69;
+        percentage[3] = 49;
+        percentage[4] = 39;
+        percentage[5] = 29;
+        percentage[6] = 19;
+        percentage[7] = 9;
+        percentage[8] = 4;
+
+        tokenToCompound[0] = 50;
+        tokenToCompound[1] = 100;
+        tokenToCompound[2] = 200;
+        tokenToCompound[3] = 300;
+        tokenToCompound[4] = 500;
+        tokenToCompound[5] = 700;
+        tokenToCompound[6] = 1000;
+        tokenToCompound[7] = 3000;
+        tokenToCompound[8] = 5000;
+
+        // 내구도, 강화내구도 수리시 토큰 소모량 의논 후 수정
+        // tokenToFixDurability[0~8] = ??;
+        // tokenToFixEnchant[0~8] = ??;
+
+        token = Token(_token);
+        
+    }
+
+    function mint (address recipient, uint256 _weaponLevel) public returns (uint256) {
     _Ids.increment();
 
     uint256 newItemId = _Ids.current();
     _mint(recipient, newItemId);
 
+    createRandomWeapon(_weaponLevel);
+
     return newItemId;
   }
 
-  // 장비 합성 추가 중
+    // 장비 합성 추가 중
+    function _createWeapon(uint256 _weaponType, uint256 _weaponLevel) internal {
+         _Ids.increment();
 
-    event NewWeapon(uint256 weaponId, uint256 weaponType);
+        uint256 id = weapons.push(Weapon(_weaponType, _weaponLevel, 3, 3)) - 1;
+        uint256 newItemId = _Ids.current();
+        
+        _mint(msg.sender, newItemId);
 
-    uint256 weaponDigits = 16;
-    uint256 weaponModulus = 10 ** weaponDigits;
-
-    struct Weapon {
-        uint256 weaponType; // dna 
-        uint256 weaponLevel; // grade, tier, rairity 등
-        uint256 durability; // 내구도
+        emit NewWeapon(id, _weaponType, _weaponLevel);
     }
 
-    Weapon[] public weapons;
-
-    mapping (uint256 => address) public weaponIndexToOwner;
-    mapping (address => uint256) ownerWeaponCount;
-
-    function _createWeapon(uint256 _weaponType) internal {
-        uint256 id = weapons.push(Weapon(_weaponType, 1, 3)) - 1;
-        WeaponIndexToOwner[id] = msg.sender;
-        ownerWeaponCount[msg.sender]++;
-        NewWeapon(id, _weaponType);
-    }
-
+    // 무기타입을 위한 랜덤 16자리 숫자 생성
     function _generateRandomType() private view returns (uint256) {
       // 블록 타임스탬프를 활용해서 랜덤한 값을 뽑아냄. 
         uint256 randomNumFromBlock = block.timestamp; 
-        uint256 randomNum = uint256(keccak256(randomNumFromBlock));
+        uint256 randomNum = uint256(keccak256(abi.encodePacked(randomNumFromBlock)));
         return randomNum % weaponModulus;
     }
 
-    function createRandomWeapon() public {
+    function createRandomWeapon(uint256 _weaponLevel) public {
         uint256 randomType = _generateRandomType();
-        randomType = randomType - randomType % 100;
-        _createWeapon(randomType);
+        randomType = randomType - randomType % 100; // 생각하기
+        uint256 id = weapons.push(Weapon(randomType, _weaponLevel, 3, 3)) - 1;
+
+        emit NewWeapon(id, randomType, _weaponLevel);
+    }
+
+    // 합성을 위한 랜덤 2자리 숫자 생성
+    function createRandomNum() public view returns(uint256) {
+        uint256 randomNumFromBlock = block.timestamp;
+        uint256 randomNum = uint256(keccak256(abi.encodePacked(randomNumFromBlock)));
+
+        return randomNum % 100;
     }
 
     function compoundWeapon(uint256 _weapon1Id, uint256 _weapon2Id) public { // targetType 없이 weaponId 2개
-    // 
-    require(msg.sender == weaponIndexToOwner[_weapon1Id]);
-    require(msg.sender == weaponIndexToOwner[_weapon2Id]);
+    // 소유한 2개의 무기만 합성 가능
+    require(msg.sender == ownerOf(_weapon1Id));
+    require(msg.sender == ownerOf(_weapon2Id));
 
     // 동일 id의 nft는 합성불가
     require(_weapon1Id != _weapon2Id , "Can not compound with Same weapon");
     
     // 동일 레벨의 무기를 합성필요 
-    Weapon storage myWeapon1 = weapons[_weaponId1];
-    Weapon storage myWeapon2 = weapons[_weaponId2];
+    Weapon storage myWeapon1 = weapons[_weapon1Id - 1];
+    Weapon storage myWeapon2 = weapons[_weapon2Id - 1];
     require(myWeapon1.weaponLevel == myWeapon2.weaponLevel, "Compound Same weapon level");
 
-    // 합성 진행시 내구도 필요
-    require(myWeapon1.durability != 0 && myWeapon2.durability != 0, "Weapon compound needs durability");
-
+    // 합성 진행시 enchant(강화 내구도) 필요
+    require(myWeapon1.enchant != 0 && myWeapon2.enchant != 0, "Weapon compound needs enchant");
     
+    // 유저가 합성시 필요한 토큰을 충분히 가졌는지 확인
+    uint256 spendToken = tokenToCompound[myWeapon1.weaponLevel - 1];
+    require(spendToken <= token.balanceOf(msg.sender), "You must have URU token to compound");
+
     // 동일 단계 또는 한 단계 위의 무기를 확률적으로 생성
+    // weapon1과의 레벨보다 하나 낮은 퍼센테이지와 랜덤숫자를 비교해서 성공, 실패 판단
+    uint256 compoundPercentage = percentage[myWeapon1.weaponLevel - 1];
+
+    uint256 newType = (myWeapon1.weaponType + myWeapon2.weaponType + _generateRandomType()) / 2 % weaponModulus;
+
+    // 합성시 enchant(강화 내구도) 1씩 소모
+    myWeapon1.enchant = myWeapon1.enchant - 1;
+    myWeapon2.enchant = myWeapon2.enchant - 1;
+
+    // 합성시 msg.sender 토큰 소모(burn)
+    token.burn(msg.sender, spendToken);
     
 
-
-    // 내구도 업데이트 
+    // 합성확률보다 랜덤숫자가 크면 실패, 작거나 같으면 성공
+    if(createRandomNum() <= compoundPercentage) {
+        _createWeapon(newType, myWeapon1.weaponLevel + 1);
+    } else {
+        _createWeapon(newType, myWeapon1.weaponLevel);
+    }
     
-
-    // approve, msg.sender 토큰 소모(burn)
-    
-    
-    uint256 newType = (myWeapon1.weaponType + myWeapon2.weaponType) / 2;
-    _createWeapon(newType);
     }
 
-    // 무기 레벨을 알려주는 함수 추가 (getter)
+    // 무기 레벨을 알려주는 함수 (getter)
+    function getWeaponLevel (uint256 _weaponId) public view returns(uint256) {
+        return weapons[_weaponId - 1].weaponLevel;
+    }
+
+    // staking 내구도 소모
+    function stakingWeapon (uint256 _weaponId) public {
+        // 스테이킹하는 무기의 주인인지 확인
+        require(msg.sender == ownerOf(_weaponId), "You have to staking your weapon");
+
+        weapons[_weaponId - 1].durability = weapons[_weaponId - 1].durability - 1;
+    }
+
+    // staking 내구도 수리
+    function fixWeaponDurability (uint256 _weaponId) public {
+        // 수리하는 무기의 소유 확인
+        require(msg.sender == ownerOf(_weaponId));
+
+        // 충분한 수리비용을 가졌는 지 확인
+        uint256 spendToken = tokenToFixDurability[myWeapon1.weaponLevel - 1];
+        require(spendToken <= token.balanceOf(msg.sender), "You must have URU token to fix durability");
+        
+        
+        weapons[_weaponId - 1].durability = 3;
+
+        token.burn(msg.sender, spendToken);
+    }
 
 
+    // enchant 내구도 수리
+    function fixWeaponEnchant (uint256 _weaponId) public {
+        // 수리하는 무기의 소유 확인
+        require(msg.sender == ownerOf(_weaponId));
+        
+        // 충분한 수리비용을 가졌는 지 확인
+        uint256 spendToken = tokenToFixEnchant[myWeapon1.weaponLevel - 1];
+        require(spendToken <= token.balanceOf(msg.sender), "You must have URU token to fix enchant");
+        
+        
+        weapons[_weaponId - 1].enchant = 3;
 
+        token.burn(msg.sender, spendToken);
+    }
 
-// 동일한 레벨의 무기 2개를 합성해서 동일 단계 또는 한단계 위의 무기를 만든다.
-// 합성할 때 uru 토큰 소모
-// 수리할 때 uru 토큰 소모
-
-
-// -  메타데이터 DNA 형식 -> 1234567890 + 1234567890 = 12 34 51 67 27 33 
-// 내구도 (클레임(합성 시도)을 할 때마다 1씩 감소)
-// 이름
-// 설명
-// 등급
-// 무기 종류
-// 이미지
-// 토큰 보상률
-
-//   - 토큰 소모
-// 무기 합성 (레벨1 ~ 10) : 합성시 각 레벨별 무기의 NFT 총 발행량을 제한
-// 레벨1: 50개 (성공확률 100%) (5000)
-// 레벨2: 100개 (80%) (5000)
-// 레벨3: 200개 (70%) (5000)
-// 레벨4: 300개 (50%) (5000)
-// 레벨5: 500개 (40%) (4000)
-// 레벨6: 700개 (30%) (4000)
-// 레벨7: 1000개 (20%) (4000)
-// 레벨8: 3000개 (10%) (3000)
-// 레벨9: 5000개 (5%) (3000)
-// 레벨10: 발행량 1000개
-// 
-// 무기 수리 / 내구도(합성가능횟수)
-// 레벨1: 50개 /  3
-// 레벨2: 100개 / 3
-// 레벨3: 200개 / 3
-// 레벨4: 300개 / 3
-// 레벨5: 500개 / 3
-// 레벨6: 700개 / 3
-// 레벨7: 1000개 / 3
-// 레벨8: 3000개 / 3
-// 레벨9: 5000개 / 3
-// 레벨10: 발행량 1000개 / 3
-
-
-
+    // 현재 msg.sender가 보유중인 무기를 배열로 리턴해주는 함수 (getter)
 
 }
