4d728236fefca5ababda1cc8e9866ab0df0018de
BEB03 <cpt9136@gmail.com>
2022-05-20 22:27:14 +0900

    feat(백동현): saving contract info to DB

--- server/controller/contracts/token.js
+++ server/controller/contracts/token.js
@@ -2,18 +2,28 @@ const tokens = require("../../models/tokens");
 
 module.exports = {
   post: async (req, res) => {
-    const { token_address, token_name, token_symbol } = req.body;
     try {
-      let tokenData = new tokens({
-        token_address: token_address,
-        token_name: token_name,
-        token_symbol: token_symbol,
+      let tokenA = new tokens({
+        token_address: req.body[0].token_address,
+        token_name: req.body[0].token_name,
+        token_symbol: req.body[0].token_symbol,
       });
-      tokenData.save((err) => {
+      let tokenB = new tokens({
+        token_address: req.body[1].token_address,
+        token_name: req.body[1].token_name,
+        token_symbol: req.body[1].token_symbol,
+      });
+      tokenA.save((err) => {
         if (err) {
           res.status(404).send({ message: "fail to saving.." });
         } else {
-          res.status(201).send({ message: "saving succeed!" });
+          tokenB.save((err) => {
+            if (err) {
+              res.status(404).send({ message: "fail to saving.." });
+            } else {
+              res.status(201).send({ message: "saving succeed!" });
+            }
+          });
         }
       });
     } catch (err) {
@@ -24,7 +34,15 @@ module.exports = {
 
   get: async (req, res) => {
     await tokens
-      .find()
+      .find(
+        {},
+        {
+          _id: false,
+          token_address: true,
+          token_name: true,
+          token_symbol: true,
+        }
+      )
       .then((data) => {
         res.status(200).json(data);
       })
